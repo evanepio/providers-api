fcb452ba2303b7d9254e69bff3efa76cd4a3dca2
Evan Porter <evan.porter@dotman.ca>
2022-07-31 18:00:01 -0300

    Don't insert crap at start up

--- providers/main.py
+++ providers/main.py
@@ -6,8 +6,3 @@ from .db.core import main as db_init
 app = FastAPI()
 
 app.include_router(company.router, prefix="/companies")
-
-
-@app.on_event("startup")
-async def startup_event():
-    db_init()
