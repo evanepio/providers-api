dd55076fd31b6bf574d811ccf5ec09b932f5a7af
Evan Porter <evan.porter@dotman.ca>
2022-08-02 19:58:09 -0300

    Fix return, and improve OpenAPI docs

--- providers/api/company.py
+++ providers/api/company.py
@@ -8,8 +8,16 @@ from ..db.models import Company
 router = APIRouter()
 
 
-@router.get("/")
-def get(db: Session = Depends(get_db)) -> list[Company]:
+@router.get("/", response_model=list[dict])
+def get(db: Session = Depends(get_db)) -> list[dict]:
     result_set = db.execute(select(Company))
 
-    return result_set.all()
+    return [
+        {
+            "id": c.Company.id,
+            "name": c.Company.name,
+            "slug": c.Company.slug,
+            "slogan": c.Company.slogan,
+        }
+        for c in result_set.all()
+    ]
