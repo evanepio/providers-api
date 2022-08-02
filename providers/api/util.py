04ca79db6a477b3e32e4f33dac3b4af04c950991
Evan Porter <evan.porter@dotman.ca>
2022-08-02 19:57:20 -0300

    Improve open API docs

--- providers/api/util.py
+++ providers/api/util.py
@@ -5,7 +5,7 @@ from ..db.core import init_db
 router = APIRouter()
 
 
-@router.get("/init-db")
+@router.get("/init-db", response_model=bool)
 def init_db_api() -> bool:
     init_db()
     return True
