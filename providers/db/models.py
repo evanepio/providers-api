13ab97b2d36be0489e5f6e52df87b2af19189e14
Evan Porter <evan.porter@dotman.ca>
2022-08-03 21:16:59 -0300

    Refactor classes according to tutorial
    We are here:
    https://sqlmodel.tiangolo.com/tutorial/fastapi/multiple-models/

--- providers/db/models.py
+++ providers/db/models.py
@@ -1,29 +1,24 @@
 from sqlmodel import Field, SQLModel, UniqueConstraint
 
 
-class Company(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+class CompanyBase(SQLModel):
     name: str
     slug: str
     slogan: str
+
+
+class Company(CompanyBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
 
     __table_args__ = (
         UniqueConstraint("name"),
         UniqueConstraint("slug"),
     )
 
-    def __repr__(self):
-        return f"{self.__name__}(id={self.id!r}, name={self.name!r})"
+
+class CompanyCreate(CompanyBase):
+    pass
 
 
-class CompanyCreate(SQLModel):
-    name: str
-    slug: str
-    slogan: str
-
-
-class CompanyRead(SQLModel):
+class CompanyRead(CompanyBase):
     id: int
-    name: str
-    slug: str
-    slogan: str
